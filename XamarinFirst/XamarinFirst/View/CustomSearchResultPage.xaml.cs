badc011480e32e156fb7f792882455400765b36e
Shakaib Akhtar <shakaib.akhtar007@gmail.com>
2020-03-16 19:08:08 +0500

    Completed Design AdvancedSearchResultPage.
    TODO:
    1- Resolve issue App crash while navigating to PropertyDetailPage

---
+++ XamarinFirst/XamarinFirst/View/CustomSearchResultPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFirst.ViewModel;
+
+namespace XamarinFirst.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomSearchResultPage : ContentPage
+    {
+        public CustomSearchResultPage()
+        {
+            InitializeComponent();
+            BindingContext = new CustomSearchResultViewModel();
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return; // catch deselection
+            }
+            ((ListView)sender).SelectedItem = null;
+            await Navigation.PushAsync(new PropertyDetailPage());
+        }
+    }
+}
